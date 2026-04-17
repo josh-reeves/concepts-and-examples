ed910ee61bac17d0d0f6afbab90d87249bc77d86
Joshua Reeves <145219625+josh-reeves@users.noreply.github.com>
2026-04-16 23:05:37 -0500

    Added examples for anonymous pipes in C#.

---
+++ c#/anonymous-pipes/client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Pipes;
+
+namespace client;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length <= 0)
+        {
+            return;
+            
+        }
+
+        AnonymousPipeClientStream clientStream = new(PipeDirection.In, args[0]);
+
+        Console.WriteLine($"[CLIENT] Current transmission mode: {clientStream.TransmissionMode}");
+
+        StreamReader streamReader = new StreamReader(clientStream);
+
+        string msg;
+
+        while (!string.IsNullOrEmpty(msg = streamReader.ReadLine() ?? string.Empty))
+        {
+            Console.WriteLine("[CLIENT] Echo: " + msg);
+            
+        }
+
+        Console.WriteLine("[CLIENT] Press Enter to continue...");
+        Console.ReadLine();
+
+
+    }
+
+}
---
+++ c#/anonymous-pipes/server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace server;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            AnonymousPipeServerStream serverStream = new(PipeDirection.Out, HandleInheritability.Inheritable);
+            StreamWriter writer = new StreamWriter(serverStream) 
+            { 
+                AutoFlush = true 
+                
+            };
+            Process client = new Process()
+            {
+                StartInfo = new()
+                {
+                    FileName = "client",
+                    UseShellExecute = false,
+                    Arguments = serverStream.GetClientHandleAsString()
+
+                }
+
+            };
+
+            client.Start();
+
+            serverStream.DisposeLocalCopyOfClientHandle();
+
+            Console.WriteLine($"[SERVER] Current transmission mode: {serverStream.TransmissionMode}");
+
+            Console.WriteLine("[SERVER] Enter text: ");
+            writer.WriteLine(Console.ReadLine());
+
+            client.WaitForExit();
+            client.Close();
+
+            Console.WriteLine("[SERVER] Client terminated. Exiting server.");
+
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine(ex);
+
+        }
+
+    }
+
+}
